e9f34284842d8dc2f7fb69dddb4fb417abc7dfac
Damir Shaikhutdinov <lost404@gmail.com>
2026-05-23 20:25:57 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "FD Shell — security-oriented shell with fd passing.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, ... }: let
+    inherit (nixpkgs) lib;
+    eachSystem = lib.genAttrs lib.systems.flakeExposed;
+    version = (builtins.fromTOML (builtins.readFile ./safe/fdshell/Cargo.toml)).package.version;
+    pkgsFor = eachSystem (system:
+      import nixpkgs {
+        localSystem.system = system;
+        overlays = [(import rust-overlay)];
+      });
+  in {
+    packages = eachSystem (system: {
+      default = pkgsFor.${system}.rustPlatform.buildRustPackage {
+        pname = "fdshell";
+        inherit version;
+        src = lib.cleanSource ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+        meta.mainProgram = "fdshell";
+      };
+    });
+
+    checks = eachSystem (system: {
+      default = self.packages.${system}.default;
+    });
+  };
+}
